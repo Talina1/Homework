42cef451b18dbcdc46e10b6cf3921e573181ba39
Lina <taraseviciene.lina@gmail.com>
2022-03-22 13:38:35 +0200

    Initial Commit: Homework

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// Homework
+
+        int Salary = 955;
+        double Taxes = Salary * 0.25;
+        System.out.println("Taxes takes 25% of Salary: " + Taxes);
+    }
+}
